63d290c9994c00ff1e35f6f626389c0ecf8bfc7d
Rizwan Arif <xrizwanr@gmail.com>
2020-07-28 22:27:07 +0500

    Updates

---
+++ precache-manifest.1f78fc55e253c8c56e2bc30635fabb29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90957a68b31c246be5de095fd9383545",
+    "url": "/react-library/index.html"
+  },
+  {
+    "revision": "588220666c3660f7682e",
+    "url": "/react-library/static/css/main.b529cb0e.chunk.css"
+  },
+  {
+    "revision": "6186919cad12a6868f75",
+    "url": "/react-library/static/js/2.588c14a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-library/static/js/2.588c14a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "588220666c3660f7682e",
+    "url": "/react-library/static/js/main.72c587bd.chunk.js"
+  },
+  {
+    "revision": "b4f9e0272f42d282aa06",
+    "url": "/react-library/static/js/runtime-main.f77d38fc.js"
+  }
+]);
